01a378fe8e25de36e14b654e17629456a7956b00
a-842 <131918994+a-842@users.noreply.github.com>
2024-04-30 09:19:49 +0100

    j

--- about/games/game-db.js
+++ about/games/game-db.js
@@ -1,10 +1,11 @@
-//{title: "", img: "", type:"[video/board]", genre"", playtime:"[very little/a bit/a moderate amount/a lot/too much]", rating:"[bad/alright/good/very good/amazing]" description:""},
+//{title: "", img: "", type:"[video/board]", genre"", playtime:"[very_little/a_bit/a_moderate_amount/a_lot/too_much]", rating:"[bad/alright/good/very_good/amazing]" description:""},
 
 
 document.addEventListener("DOMContentLoaded", function() {
   	const games = [
-		{title: "Cuphead", img: "cuphead-header.jpeg", type:"video", genre"", playtime:"a moderate amount", rating:"very good" description:"An amazing indie shoot 'em up to relax with and bring a sense of nostalgia from the graphics and soundtrack"},
-
+		{title: "Cuphead", img: "cuphead-header.jpeg", type:"video", genre"run_&_gun", playtime:"a_moderate_amount", rating:"very_good" description:"An amazing indie shoot 'em up to relax with and bring a sense of nostalgia from the graphics and soundtrack."},
+		{title: "Hollow Knight", img: "hollow-knight-header.jpg", type:"video", genre"metroidvania", playtime:"too much", rating:"amazing" description:"The First real pc game I played, and I still come back to this very fondly - but only with some of the skills I mastered first time round!"},
+		{title: "Dominion_-Prosperity", img: "dominion-prosperity-header.jpg", type:"board", genre"deck_building", playtime:"a_lot", rating:"very good" description:"The Colonies and Platinums extend the game to create absurd builds that are infeasible with other sets."},
 	];
 
   var container = document.getElementById("cards-container");
