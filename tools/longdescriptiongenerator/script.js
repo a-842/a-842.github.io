bc15cf6d0b7e2edefd10a7970bf3f975af541824
a-842 <131918994+a-842@users.noreply.github.com>
2024-05-02 09:02:48 +0100

    try -

--- tools/longdescriptiongenerator/script.js
+++ tools/longdescriptiongenerator/script.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function() {
     removePretextButton.addEventListener("click", function removePretext() {
         var lines = inputText.value.split("\n");
         for (line in lines) {
-            if (line.startsWith("•")) {
+            if (line.startsWith("-")) {
                 line = line.substring(1).trim();
             }
             
