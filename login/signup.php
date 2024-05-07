746d225c99a6e17db1d5d1be730d8b27e08b8552
a-842 <131918994+a-842@users.noreply.github.com>
2024-05-07 17:40:45 +0100

    Update signup.php

--- login/signup.php
+++ login/signup.php
@@ -8,4 +8,23 @@ if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
   die("Email in wrong format");
 }
 
-if ()
+if (strlen($_POST["password"]) < 8) {
+  die("Pass word must be at least 8 Characters");
+}
+
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+  die("Password must contain at least one number");
+}
+
+if ( ! preg_match("/[a-z]/", $_POST["password"])) {
+  die("Password must contain at least one letter");
+}
+
+if ( $_POST["password"] !== $_POST["password_confirm"]) {
+  die("Passwords must match");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+print_r($_POST);
+var_dump($password_hash);
