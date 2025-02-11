c2c8b84fe45a56fe5a98c0e5e4dbb7af65102796
a-842 <131918994+a-842@users.noreply.github.com>
2025-02-11 08:30:11 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,8 @@
 
-# import flask module
 from flask import Flask
- 
-# instance of flask application
+
 app = Flask(__name__)
- 
-# home route that returns below text 
-# when root url is accessed
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
